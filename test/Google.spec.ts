973388769031e8bd8d5dfc867671345a0b301c99
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2017-10-10 11:05:47 -0500

    Agregando Análisis de Código Estático (#8)
    * Agregando Análisis de Código Estático
    - npm install  tslint  tslint-config-airbnb
    - npm run lint -- --fix
    - npm test
    
    * Agregando Análisis de Código Estático
    +    "protractor": "^5.1.2",
    +    "tslint": "^5.7.0",
    estas dos deberias ser dev dependencies
    
    * Agregando Análisis de Código Estático
    - las 2 librerias que no deberían estar en dependencias eran las dos nuevas que agregaste, en el caso de protractor si debe estar en dependencias

--- test/Google.spec.ts
+++ test/Google.spec.ts
@@ -1,7 +1,7 @@
 import { browser } from 'protractor' ;
 
-describe( 'Given a SDET learning protractor' , () => {
-  describe( 'when open Google Page' , () => {
+describe('Given a SDET learning protractor' , () => {
+  describe('when open Google Page' , () => {
     beforeEach(async () => {
       await browser.get('http://www.google.com');
     });
